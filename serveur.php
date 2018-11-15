2cba47560422ac6baa3cdfee12dc5b2ce5f431f9
DEPAULISHugo <hugo.de-paulis@ensg.eu>
2018-11-15 11:51:27 +0100

    Ajout du fichier PHP

---
+++ serveur.php
@@ -0,0 +1,14 @@
+<?php
+if (isset($_SESSION['id']) && !(empty($_SESSION['id']))){
+  $link = pg_connect("port=5432 dbname=postgis_24_sample user=postgres password=postgres");
+  echo "<p>Connexion établie</p>";
+  $idObjet = $_SESSION["id"];
+  $requete = "SELECT srid, auth_name FROM spatial_ref_sys LIMIT 20";
+  if ($result = pg_query($link, $requete)) {
+    while ($row = pg_fetch_row($result)) {
+      echo "Auteur: $row[0]  E-mail: $row[1]";
+      echo "<br />\n";
+    }
+  }
+}
+?>
