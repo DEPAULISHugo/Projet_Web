502999dde789fc15701d522c8b86280608eb5c91
shuthello <hugo.depaulis@gmail.com>
2018-11-15 21:35:27 +0100

    MAJ de la classe objet et de server

--- objet.class.php
+++ objet.class.php
@@ -1,10 +1,30 @@
 <?php
   class Objet{
+
     public $id;
     public $name;
     public $lat;
     public $lng;
     public $minZoom;
     public $icone;
-  }
+
+    public function __construct($id,  $name, $lat, $lng, $minZoom, $icone){
+      $this->id = $id;
+      $this->name = $name;
+      $this->lat = $lat;
+      $this->lng = $lng;
+      $this->minZoom = $minZoom;
+      $this->icone = $icone;
+    }
+}
+
+/*$objet = new Objet(1, "nom", 50.123, 45.987, 5, "icone.png");
+
+echo "<p>Nom : " . $objet->id . "</p>";
+echo "<p>Nom : " . $objet->name . "</p>";
+echo "<p>Latitude : " . $objet->lat . "</p>";
+echo "<p>Longitude : " . $objet->lng . "</p>";
+echo "<p>Zoom minimal : " . $objet->minZoom . "</p>";
+echo "<p>Icone : " . $objet->icone . "</p>";
+*/
 ?>
--- serveur.php
+++ serveur.php
@@ -1,8 +1,23 @@
 <?php
 
+include("objet.class.php");
+
+function getObjetFromDB($id) {
+  $link = pg_connect("port=5432 dbname=projet user=postgres password=postgres");
+  echo "<p>Connexion établie</p>";
+  $idObjet = $_SESSION["id"];
+  $requete = "SELECT * FROM objet WHERE id={$id}";
+  if ($result = pg_query($link, $requete)) {
+    while ($row = pg_fetch_row($result)) {
+      echo new Objet(row[0], row[1], row[2], row[3], row[4], row[5]);
+    }
+  }
+}
+
+
 $_SESSION['id'] = 1;
 
-if (isset($_SESSION['id']) && !(empty($_SESSION['id']))){
+/*if (isset($_SESSION['id']) && !(empty($_SESSION['id']))){
   $link = pg_connect("port=5432 dbname=postgis_24_sample user=postgres password=postgres");
   echo "<p>Connexion établie</p>";
   $idObjet = $_SESSION["id"];
@@ -13,5 +28,5 @@ if (isset($_SESSION['id']) && !(empty($_SESSION['id']))){
       echo "<br />\n";
     }
   }
-}
+}*/
 ?>
