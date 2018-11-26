45cbc981945c58f424add962680161efd5235792
DEPAULISHugo <hugo.de-paulis@ensg.eu>
2018-11-26 19:29:13 +0100

    Petites corrections pour l'interaction avec les objets

--- serveur.php
+++ serveur.php
@@ -19,11 +19,35 @@ function getObjetFromDB($id) {
   }/*else{
     echo "<p>Requête echouée</p>";
   }*/
-  return $objet;
+  return $objet->toString();
+}
+
+function getObjetBloquantFromDB($idBloque) {
+  $link = pg_connect("host=localhost port=5432 dbname=projet user=postgres password=postgres");
+  /*if(!$link){
+    die("Erreur de connexion");
+  }else{
+    echo "<p>Connexion établie</p>";
+  }*/
+  $requete = "SELECT o.id, o.nom, o.latitude, o.longitude, o.zoom, o.icone, o.texte FROM objets AS o, lienObjets AS l WHERE l.idBloquant=o.id AND l.idBloque=".$idBloque;
+  $result = pg_query($link, $requete);
+  if ($result) {
+    /*echo "<p>Requête réussie</p>";*/
+    while ($row = pg_fetch_row($result)) {
+      $objet = new Objet ($row[0], $row[1], $row[2], $row[3], $row[4], $row[5], $row[6]);
+    }
+  }else{
+    return "";
+  }
+  return $objet->toString();
 }
 
 if (isset($_GET['id'])){
-  echo getObjetFromDB($_GET['id'])->toString();
+  echo getObjetFromDB($_GET['id']);
+}
+
+if (isset($_GET['idBloque'])){
+  echo getObjetBloquantFromDB($_GET['idBloque']);
 }
 
 /*echo getObjetFromDB(0)->toString();*/
