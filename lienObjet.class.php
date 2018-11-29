502b65ba358ae586af22c7b8368467de9baadc9b
DEPAULISHugo <hugo.de-paulis@ensg.eu>
2018-11-29 14:47:07 +0100

    Modification de action()

---
+++ lienObjet.class.php
@@ -0,0 +1,18 @@
+<?php
+  class LienObjet{
+
+    public $idBloque;
+    public $idBloquant;
+    public $texteDebloque;
+
+    public function __construct($idBloque, $idBloquant, $texteDebloque){
+      $this->idBloque = $idBloque;
+      $this->idBloquant = $idBloquant;
+      $this->texteDebloque = $texteDebloque;
+    }
+
+    public function toString(){
+      return '{"idBloque":'.$this->idBloque.', "idBloquant":'.$this->idBloquant.', "texteDebloque":"'.$this->texteDebloque.'"}';
+    }
+}
+?>
