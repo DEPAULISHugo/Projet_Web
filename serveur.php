071cd03c3b6f054d2c14323c3f3ec300be45f15e
DEPAULISHugo <hugo.de-paulis@ensg.eu>
2018-11-29 14:57:46 +0100

    Correction du serveur PHP

--- serveur.php
+++ serveur.php
@@ -29,17 +29,13 @@ function getObjetBloquantFromDB($idBloque) {
   }else{
     echo "<p>Connexion établie</p>";
   }*/
-  $requete = "SELECT o.id, o.nom, o.latitude, o.longitude, o.zoom, o.icone, o.texte FROM objets AS o, lienObjets AS l WHERE l.idBloquant=o.id AND l.idBloque=".$idBloque;
+  $requete = "SELECT idBloque, idBloquant, texteDebloque FROM lienObjets WHERE idBloque=".$idBloque;
   $result = pg_query($link, $requete);
-  if ($result) {
-    /*echo "<p>Requête réussie</p>";*/
-    while ($row = pg_fetch_row($result)) {
-      $objet = new Objet ($row[0], $row[1], $row[2], $row[3], $row[4], $row[5], $row[6]);
-    }
-  }else{
-    return "";
+  $lienObjet = new LienObjet (-1, -1, null);
+  while ($row = pg_fetch_row($result)) {
+    $lienObjet = new LienObjet ($row[0], $row[1], $row[2]);
   }
-  return $objet->toString();
+  return $lienObjet->toString();
 }
 
 if (isset($_GET['id'])){
@@ -49,19 +45,4 @@ if (isset($_GET['id'])){
 if (isset($_GET['idBloque'])){
   echo getObjetBloquantFromDB($_GET['idBloque']);
 }
-
-/*echo getObjetFromDB(0)->toString();*/
-
-/*if (isset($_SESSION['id']) && !(empty($_SESSION['id']))){
-  $link = pg_connect("port=5432 dbname=postgis_24_sample user=postgres password=postgres");
-  echo "<p>Connexion établie</p>";
-  $idObjet = $_SESSION["id"];
-  $requete = "SELECT srid, auth_name FROM spatial_ref_sys LIMIT 20";
-  if ($result = pg_query($link, $requete)) {
-    while ($row = pg_fetch_row($result)) {
-      echo "Auteur: $row[0]  E-mail: $row[1]";
-      echo "<br />\n";
-    }
-  }
-}*/
 ?>
